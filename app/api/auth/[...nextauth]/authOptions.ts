6d7682dda1b1c6cfcbec0adb829ca73fec51bdd8
Omar Pervez <freelancertusar5182@gmail.com>
2026-05-15 01:51:35 +0600

    added google option

--- app/api/auth/[...nextauth]/authOptions.ts
+++ app/api/auth/[...nextauth]/authOptions.ts
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import GitHubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
 import type { NextAuthOptions } from "next-auth";
 
 export const authOptions: NextAuthOptions = {
@@ -8,16 +9,25 @@ export const authOptions: NextAuthOptions = {
       clientId: process.env.GITHUB_ID!,
       clientSecret: process.env.GITHUB_SECRET!,
     }),
+
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
   ],
+
   secret: process.env.NEXTAUTH_SECRET,
+
   callbacks: {
     async session({ session, token }) {
       if (session.user && token.sub) {
         (session.user as any).id = token.sub;
       }
+
       return session;
     },
   },
+
   pages: {
     signIn: "/signin",
   },
